3ef3a704bbec53cde6dfd039b1e990d04dd2fc20
MrTorettoKid <127558760+MrTorettoKid@users.noreply.github.com>
2024-08-20 12:14:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,8 +56,8 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "nanolinks.in") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "629c0b32c3b6b22ba48cb39a4e7ec5617dcbe67d") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
 
 # Website Info:
